69f05befc75b9a94dd28df36dc4ed48b0691b607
Prasanna Boga <prasannaboga@gmail.com>
2020-07-30 14:27:41 +0530

    Added new site tools

--- config.rb
+++ config.rb
@@ -40,7 +40,15 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  set :relative_links, true
+  activate :minify_css
+  activate :minify_javascript  
+end
+
+activate :livereload
+
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  deploy.branch = 'gh-pages'
+end
